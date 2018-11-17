3a3a9b02b93a9368d72c5dd524a09cad3b6d9bba
jadenswang <jaden.shiteng.wang@gmail.com>
2018-11-16 23:31:05 -0500

    board.py completed

--- src/Board.py
+++ src/Board.py
@@ -1,25 +1,17 @@
-def check_rows(board):
-    for row in board:
-        index = 1
-        front = row[0]
-        while index < len(board):
-            if front != row[index]:
-                return False
-            index += 1
-
+def check_row(board):
+    front = board[0]
+    for number in board:
+        if number != front:
+            return False
     return True
 
 
-def check_cols(board):
-    row = 0
+def check_col(board, col):
+    front = board[0][col]
+    row = 1
     while row < len(board):
-        front = board[row][0]
-        col = 1
-        while col < len(board[row]):
-            if board[row][col] != front:
-                return False
-            col += 1
-
+        if board[row][col] != front:
+            return False
         row += 1
 
     return True
@@ -51,15 +43,24 @@ def check_back_diag(board):
 
 class Board:
     def __init__(self):
-        self.board = [["", "", ""], ["", "", ""], ["", "", ""]]
+        self.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
         self.player_ones_turn = True
         self.moves_made = 0
 
     def player_ones_turn(self):
         return self.player_ones_turn
 
-    def make_move(self):
+    def make_move(self, row, col):
+        if self.board[row][col] != '-':
+            raise TypeError
+
         self.moves_made += 1
+        if self.player_ones_turn:
+            self.board[row][col] = "x"
+        else:
+            self.board[row][col] = "o"
+
+        self.player_ones_turn = not self.player_ones_turn
 
     def get_board(self):
         return self.board
@@ -68,5 +69,38 @@ class Board:
         if self.moves_made < 5:
             return False
 
-        return check_rows(self.board) and check_cols(self.board) and check_forward_diag(self.board) and check_back_diag(
-            self.board)
+        for row in self.board:
+            if check_row(row):
+                return True
+
+        col = 0
+        while col < len(self.board[col]):
+            if check_col(self.board, col):
+                return True
+            col += 1
+
+        return check_forward_diag(self.board) and check_back_diag(self.board)
+
+    def print_board(self):
+        print("    1    2    3")
+        for row in self.board:
+            index = 0
+            to_print = "" + str(index) + "   "
+            while index < len(row):
+                to_print += row[index] + "    "
+                index += 1
+            print(to_print)
+
+
+t = Board()
+
+t.print_board()
+t.make_move(0, 0)
+t.make_move(0, 1)
+t.make_move(1, 0)
+t.make_move(1, 1)
+t.make_move(2, 0)
+print(t.player_one_has_won())
+t.print_board()
+
+
